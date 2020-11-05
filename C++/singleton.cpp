5b0c3871df818e05f0c2cadab524b595392ef146
chengqian <cheng,qian@elektrobit.com>
2020-11-05 14:24:16 +0800

    add singleton

--- C++/singleton.cpp
+++ C++/singleton.cpp
@@ -1,26 +1,31 @@
-    #include "singleton.hpp"
-    namespace cc{
-    SingleTon* SingleTon::m_pInstance = nullptr;
-    std::mutex SingleTon::m_mutex;
-    SingleTon* SingleTon::getinstance(){
-        if (nullptr==m_pInstance){
-            std::lock_guard<std::mutex> guard(m_mutex);
-            if (nullptr == m_pInstance){
-                m_pInstance = new SingleTon();
-            }
-        }
-        return m_pInstance;
-    }
-    void SingleTon::destoryInstance(){
-        if (m_pInstance!= nullptr){
-            delete m_pInstance;
-            m_pInstance = nullptr;
-        }
-    }
-    
-    Single2 Single2::m_Instance;
-    Single2 & Single2::getInstance(){
-        return m_Instance;
-    }
+
+
+#include<stdio.h>
+#include "assert.h"
+class Singleton
+{
+    public:
+    static Singleton& Instance()
+    {
+        static Singleton instance;
+        return instance;
 
     }
+    void print()
+    {
+        printf("my address:%p\n",this);
+    }
+    private:
+    Singleton(){}
+    Singleton(const Singleton&);
+    Singleton& operator=(const Singleton&);
+
+};
+
+int main()
+{
+    Singleton::Instance().print();
+    Singleton::Instance().print();
+
+
+}
